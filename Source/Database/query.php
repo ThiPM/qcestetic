25ae6e7fd53f81d4a164b4646d0a0144739cdf12
ThiPM <thiago.martins@aluno.ifsp.edu.br>
2022-06-13 23:27:50 -0300

    msg de envio 15

--- Source/Database/query.php
+++ Source/Database/query.php
@@ -12,11 +12,9 @@ INSERT INTO users (name, email, message) VALUES ('$name', '$email', '$message');
 
 try {
     $query = $conn->query(($insert));
-    header("Location: ../../views/contato.php");
     $_SESSION['msg'] = "<center><p style='padding: 10px; border: 1px solid gray; border-radius: 3px; margin: 10px;
     font-size: 18px; border-color: #87c940; color: #fff; background-color: #a0d468; width: 290px;'>Mensagem enviada com sucesso!</p></center><br>";
 } catch (PDOException $exception) {
-    header("Location: ../../views/contato.php");
     $_SESSION['msg'] = "<center><p style='padding: 10px; border: 1px solid gray; border-radius: 3px; margin: 10px;
     font-size: 18px; border-color: #e8273b; color: #fff; background-color: #ed5565;'>Mensagem nПлкo enviada! . {$exception}</p></center><br>";
 }
