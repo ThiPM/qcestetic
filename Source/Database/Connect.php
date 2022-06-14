6f2b9d7e18fea0fe5c72e940a523b0e1b3997a8c
ThiPM <thiago.martins@aluno.ifsp.edu.br>
2022-06-13 22:39:50 -0300

    msg de envio 5

--- Source/Database/Connect.php
+++ Source/Database/Connect.php
@@ -17,3 +17,11 @@ try {
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
+
+$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+$message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING);
+
+$insert = "
+INSERT INTO users (name, email, message) VALUES ('$name', '$email', '$message');
+";
