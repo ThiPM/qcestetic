53e071bd36884f02b2cc6884351c415d622975e4
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-06-17 21:09:57 -0300

    Update Front

--- views/contato.php
+++ views/contato.php
@@ -47,11 +47,11 @@
   </div>
   <div class="col-md-6">
     <label class="form-label">Email:</label>
-    <input type="email" name="email" required="required" placeholder="Digite aqui o seu email..." id="campo_email" onfocus="clicou_campo_email()" onblur="nao_clicou_campo_email()" class="form-control">
+    <input type="email" name="email" required="required" placeholder="Digite aqui o seu email..." id="campo_email" class="form-control">
   </div><br><br><br><br>
   <div class="col-md-6">
     <label class="form-label">Mensagem:</label>
-    <textarea type="text" maxlength="1000" required="required" minlength="1" name="message" style="height: 150px;" placeholder="Digite aqui a sua mensagem..." id="campo_msg" onfocus="clicou_campo_msg()" onblur="nao_clicou_campo_msg()" class="form-control"></textarea>
+    <textarea type="text" maxlength="1000" required="required" minlength="1" name="message" style="height: 150px;" placeholder="Digite aqui a sua mensagem..." id="campo_msg" class="form-control"></textarea>
   </div>
   <div class="col-12">
   <br><center><button type="submit" id="bttn_enviar" class="btn btn-primary">Enviar</button></center><br><br>
