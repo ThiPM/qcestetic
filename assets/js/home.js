6931749b83a6556d7a4de8b36a05eeb3ea5495d2
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-08-20 18:16:44 -0300

    Estruturação da página

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+function entrou_carrossel() {
+    document.getElementById("botao_anterior").style.display = "block";
+    document.getElementById("botao_proxima").style.display = "block";
+  }
+  function saiu_carrossel() {
+    document.getElementById("botao_anterior").style.display = "none";
+    document.getElementById("botao_proxima").style.display = "none";
+  }
