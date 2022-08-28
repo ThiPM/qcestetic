7d69cb9f87b044cbc6de538b5b3700788af1b798
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-08-28 19:56:21 -0300

    Correções de redirect

--- Source/Database/query.php
+++ Source/Database/query.php
@@ -14,9 +14,9 @@ try {
     $query = $conn->query(($insert));
     $_SESSION['msg'] = "<center><p style='padding: 10px; border: 1px solid gray; border-radius: 3px; margin: 10px;
     font-size: 18px; border-color: #87c940; color: rgb(48, 48, 48); background-color: #a0d468; width: 290px;'>Mensagem enviada com sucesso!</p></center><br>";
-    header("Location: ../../#msg");
+    header("Location: ../../#form_ctt");
 } catch (PDOException $exception) {
     $_SESSION['msg'] = "<center><p style='padding: 10px; border: 1px solid gray; border-radius: 3px; margin: 10px;
     font-size: 18px; border-color: #e8273b; color: rgb(48, 48, 48); background-color: #ed5565;'>Mensagem não enviada! . {$exception}</p></center><br>";
-    header("Location: ../../#msg");
+    header("Location: ../../#form_ctt");
 }
