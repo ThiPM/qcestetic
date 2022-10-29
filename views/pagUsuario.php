1bfb084df09578cb27591591b00dc09fb658aa82
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-10-28 22:09:51 -0300

    Excluir conta (feito)

--- views/pagUsuario.php
+++ views/pagUsuario.php
@@ -71,7 +71,7 @@
 <h6 id="name_user"><?php echo $nome?></h6><br>
 
 <!-- Troca de senha fixa -->
-<button class="btn btn-primary" id="btnTrocaSenha" type="button">Trocar senha</button><br><br>
+<button class="btn btn-primary" id="btnTrocaSenha" type="button">Trocar senha</button><br>
 <div class="col-12" id="troca_senhaModal">
 <p class="textos">Digite a sua nova senha de acesso.</p>
 <form id="form_trocaSenha">
@@ -86,12 +86,20 @@
 </div>
 <!-- -->
 
+<p class="button_more_config" id="btnMoreConfig" type="button"> Configurações Avançadas
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+</svg>
+</p>
+
+<button class="button_deletarConta" id="btnDeletarConta" type="button">Excluir conta</button><br>
+
 <button class="button_logoff" id="btnLogoff" type="button">Sair<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M2 2.75C2 1.784 2.784 1 3.75 1h2.5a.75.75 0 010 1.5h-2.5a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h2.5a.75.75 0 010 1.5h-2.5A1.75 1.75 0 012 13.25V2.75zm6.56 4.5l1.97-1.97a.75.75 0 10-1.06-1.06L6.22 7.47a.75.75 0 000 1.06l3.25 3.25a.75.75 0 101.06-1.06L8.56 8.75h5.69a.75.75 0 000-1.5H8.56z"></path></svg></button><br>
 
 
 </div>
 </div>
-</div>
+</div><br><br>
 
 <div class="container" id="controle_usuario">
 
@@ -107,7 +115,7 @@
 
 <p class="textos">Aqui é sua área de usuário. Você poderá gerenciar seus agendamentos nesta tela. Caso tenha alguma dúvida entre em contato conosco e teremos prazer em ajudá-lo.</p>
 
-<a href="#">
+<a href="agendamentoUsuario.php">
 <button id="marcar_consulta_button">
 <span>Marcar uma consulta...</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
 </button>
@@ -135,6 +143,23 @@
       </div>
       <div class="modal-footer">
         <a href="login.php"><button class="btn btn-primary" id="btnLoginRedirect" type="button">Login</button></a><br><br>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 2 -->
+<div class="modal fade" id="deletarContaModalConfirm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Até mais! :(</h5>
+      </div>
+      <div class="modal-body">
+        <p>Sua conta foi excluída com sucesso.</p>
+      </div>
+      <div class="modal-footer">
+        <a href="logout.php"><button class="btn btn-primary" id="btnLoginRedirect" type="button">Voltar</button></a><br><br>
       </div>
     </div>
   </div>
