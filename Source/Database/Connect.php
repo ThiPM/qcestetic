72b01163eeace191d8942b275093e6fd18c3cd61
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-10-29 21:05:45 -0300

    Config Hospedagem

--- Source/Database/Connect.php
+++ Source/Database/Connect.php
@@ -4,11 +4,11 @@ namespace source\Database;
 use \PDO;
 use \PDOException;
 
- define('HOST', 'localhost');  
- define('DBNAME', 'qcestetica');  
- define('CHARSET', 'utf8');  
- define('USER', 'root');  
- define('PASSWORD', '');  
+  define('HOST', '51.79.72.47');  
+  define('DBNAME', 'hostdeprojetos_qcestetica');  
+  define('CHARSET', 'utf8');  
+  define('USER', 'hostdeprojetos_trlsites');  
+  define('PASSWORD', 'Cq)*wcLG=!6P'); 
 
  class Connect {  
 
