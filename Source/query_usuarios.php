5a9074d0c09b832725ec5c44defd0ee2b188040e
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-11-05 11:32:07 -0300

    Atualização Tabelas Administração

---
+++ Source/query_usuarios.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+    require("../Source/Database/Connect.php");
+
+    if(!isset($_SESSION['loggedIN_admin'])){
+        header('Location: index.php');
+        exit();
+    }
+
+    $nome = $_SESSION['usuario'][0];
+    $adm = $_SESSION['usuario'][1];
+?>
+<!-- jQuery e CSS Bootstrap 5 -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- jQuery Google -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="icon" type="image/png" href="../assets/img/icon.jpg"/>
+<title>Administração - Usuários</title>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Satisfy&family=Shippori+Antique+B1&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+* {
+    font-family: 'Poppins', sans-serif;
+}
+#bttn_deleteUsuario {
+    border: none;
+}
+</style>
+
+<?php
+include_once "../Source/Database/Connect.php";
+use Source\Database\Connect;
+
+function listaUsuarios() {
+try {
+    $viewList = 
+"
+    SELECT*FROM usuarios;
+";
+    $query = Connect::getInstance()->query($viewList);
+    $retDados = $query-> fetchAll(PDO::FETCH_ASSOC);
+    return $retDados;
+} catch (PDOException $exception) {
+    die($exception);
+}
+}
+$usuarios = listaUsuarios();
+?>
+
+<table class='table table-striped table-bordered table-hover'>
+            <thead>
+                <tr class='active'>
+                    <th>Código do cliente</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Endereço</th>
+                    <th>CEP</th>
+                    <th>Cidade</th>
+                    <th>Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+               <?php foreach($usuarios as $usuarios) : ?>
+                   <tr>
+                     <td><?=$usuarios['id']?></td>
+                     <td><?=$usuarios['nome']?></td>
+                     <td><?=$usuarios['email']?></td>
+                     <td><?=$usuarios['endereco']?></td>
+                     <td><?=$usuarios['cep']?></td>
+                     <td><?=$usuarios['cidade']?></td>
+                     <td><?=$usuarios['telefone']?></td>
+                    </tr>
+               <?php endforeach; ?>
+            </tbody>
+            </table>
+            <center><a href="../admin/hiddenPage.php"><button class="btn btn-primary" style="background-color: #b520b5;">Voltar</button></a><br><br></center>
--- views/agendamentoUsuario.php
+++ views/agendamentoUsuario.php
@@ -1,7 +1,25 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION["usuario"]) && is_array($_SESSION["usuario"])){
+        require("../Source/Database/Connect.php");
+
+        $ativo  = $_SESSION["usuario"][1];
+        $nome = $_SESSION["usuario"][0];
+        $email = $_SESSION["usuario"][2];
+
+        $min = 1;
+        $max = 100000;
+        $codigo = rand($min, $max);
+    }else{
+        echo "<script>window.location = 'login.php'</script>";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<title>QC Estética - Cadastro</title>
+<title>QC Estética - Agendamento</title>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
